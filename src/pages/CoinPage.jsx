2b807b12def3237f87f2daa02738efbbba405df8
marcosamado <led_9dejulio@hotmail.com>
2024-09-18 17:00:51 -0300

    trabajando en la logica de las preguntas

--- src/pages/CoinPage.jsx
+++ src/pages/CoinPage.jsx
@@ -1,29 +1,66 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 
 const CoinPage = () => {
     const { data } = useLoaderData();
-    const { id } = useParams();
-    const [pregunta1, setPregunta1] = useState(0);
+    let { id } = useParams();
 
-    const { anverso, reverso } = data;
+    id = id - 1;
+    if (!data || typeof data !== "object") {
+        console.error("La respuesta no es válida:", data);
+        return <div>Error al cargar los datos.</div>;
+    }
+
+    class Element {
+        constructor(name, loe) {
+            this.name = name;
+            this.loe = loe; // [No, Sí] o null si es un nodo hoja
+        }
+    }
+
+    const A1 = new Element("A1", null);
+    const A2 = new Element("A2", null);
+    const A3 = new Element("A3", null);
+    const A4 = new Element("A4", null);
+    const A5 = new Element("A5", null);
+    const A6 = new Element("A6", null);
+    const A7 = new Element("A7", null);
+    const A8 = new Element("A8", null);
+    const A9 = new Element("A9", null);
+
+    const P8 = new Element("P8: idéntica a A4 excepto por 4 rayos seguidos", [
+        A4,
+        A7,
+    ]);
+    const P7 = new Element(
+        "P7: Comienzo de la segunda N de Argentina alineada con Rayo",
+        [P8, A1]
+    );
+    const P6 = new Element("P6: Rayo Grueso Desp de la C", [A2, A3]);
+    const P5 = new Element("P5: N Alineada con Rayo", [P6, P7]);
+    const P4 = new Element("P4: Largo-corto-Largo en toda la moneda", [A8, A9]);
+    const P3 = new Element("P3: 3 Largo-corto-Largo seguidos", [P5, P4]);
+    const P2 = new Element("P2: 8 Alineado con Rayo", [A6, A5]);
+    const P1 = new Element("P1: Tiene Puntos alrededor de la fecha", [P2, P3]);
+    const [stateP, setStateP] = useState(P1);
+
     const handleClickYes = () => {
-        setPregunta1(1);
+        setStateP(stateP.loe[1]);
     };
 
     const handleClickNo = () => {
-        setPregunta1(2);
+        setStateP(stateP.loe[0]);
     };
     return (
         <div className="max-w-3xl ml-auto mr-auto">
             <h1 className="mt-10 font-bold text-3xl bg-gradient-to-r from-zinc-800 to-indigo-600 bg-clip-text text-transparent">
-                Moneda de 4 centavos de 1854
+                {data[id].nombre}
             </h1>
             <div className="flex flex-row justify-around mt-10">
                 <div>
                     <h2 className="text-4xl ">Anversos</h2>
                     <ul className="text-xl mt-5 ">
-                        {anverso?.map((element, index) => (
+                        {data[id].anverso.map((element, index) => (
                             <li key={index}>{element}</li>
                         ))}
                     </ul>
@@ -35,15 +72,15 @@ const CoinPage = () => {
                     </button>
                     <div className="flex flex-row text-center mt-5 gap-3">
                         <p className="font-semibold text-xl self-center">
-                            Hay puntos a los lados de la fecha?
+                            {stateP.name}
                         </p>
-                        <div className="w-36 h-10">
+                        {/* <div className="w-36 h-10">
                             <img
                                 className="w-full h-full"
                                 src="images/A3.jpg"
                                 alt=""
                             />
-                        </div>
+                        </div> */}
 
                         <button
                             onClick={handleClickYes}
